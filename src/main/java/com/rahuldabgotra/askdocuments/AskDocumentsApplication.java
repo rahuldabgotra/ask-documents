6b70e6ed7160f722c82bd1e3b8b1440051a00bbf
Rahul Dabgotra <rahuldabgo@gmail.com>
2024-03-22 06:00:31 +0530

    Initial commit

---
+++ src/main/java/com/rahuldabgotra/askdocuments/AskDocumentsApplication.java
@@ -0,0 +1,13 @@
+package com.rahuldabgotra.askdocuments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskDocumentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahuldabgotra/askdocuments/AskDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahuldabgotra.askdocuments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
