0f79c6bd035e2331b452b93c827dfa696bcf6207
Rahul Dabgotra <rahuldabgo@gmail.com>
2024-03-27 04:13:10 +0530

    Update AskDocumentsApplication.java

--- src/main/java/com/rahuldabgotra/askdocuments/AskDocumentsApplication.java
+++ src/main/java/com/rahuldabgotra/askdocuments/AskDocumentsApplication.java
@@ -1,10 +1,42 @@
 package com.rahuldabgotra.askdocuments;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.loadDocument;
+
 @SpringBootApplication
 public class AskDocumentsApplication {
+
+	private final EmbeddingStoreIngestor embeddingStoreIngestor;
+
+	public AskDocumentsApplication(EmbeddingStoreIngestor embeddingStoreIngestor) {
+		this.embeddingStoreIngestor = embeddingStoreIngestor;
+	}
+
+	@PostConstruct
+	public void init() {
+		Document document = loadDocument(toPath("spring-boot-reference.pdf"), new ApachePdfBoxDocumentParser());
+		embeddingStoreIngestor.ingest(document);
+	}
+
+	private static Path toPath(String fileName) {
+		try {
+			URL fileUrl = AskDocumentsApplication.class.getClassLoader().getResource(fileName);
+			return Paths.get(fileUrl.toURI());
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AskDocumentsApplication.class, args);
